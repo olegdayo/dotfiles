8b7effc5fe248bba563fa0d1e3b52729849bcdbc
Oleg Sidorenkov <ol-sidorenkov@mail.ru>
2025-04-15 11:18:14 +0300

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,9 +1,9 @@
 local wezterm = require 'wezterm'
-
 local config = wezterm.config_builder()
 
--- config.color_scheme = 'AdventureTime'
-config.font_size = 10
+config.scrollback_lines = 100000
+
+config.font_size = 9
 config.keys = {
     {
       key = '\\',
